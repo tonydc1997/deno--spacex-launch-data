ff38be101d7878400fa11651d8ea2fcc00c7de3f
Anthony Curtis <tonydc1997@gmail.com>
2020-07-07 12:31:42 -0400

    feat: Play around with log.info

--- mod.ts
+++ mod.ts
@@ -1,12 +1,13 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 
 async function downloadLaunchData() {
+  log.info("Beginning download...");
   const response = await fetch("https://api.spacexdata.com/v4/launches", {
     method: "GET",
   });
 
   const launchData = await response.json();
-  console.log(launchData);
+  // console.log(launchData);
 }
 
 downloadLaunchData();
