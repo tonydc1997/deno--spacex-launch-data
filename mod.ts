5d42dd18216282502d6c3d0e69164d1d7fe7c0c1
Anthony Curtis <tonydc1997@gmail.com>
2020-07-07 12:11:50 -0400

    feat: Parse text as json, then log it to the console

--- mod.ts
+++ mod.ts
@@ -2,4 +2,7 @@ async function downloadLaunchData() {
   const response = await fetch("https://api.spacexdata.com/v4/launches", {
     method: "GET",
   });
+
+  const launchData = await response.json();
+  console.log(launchData);
 }
