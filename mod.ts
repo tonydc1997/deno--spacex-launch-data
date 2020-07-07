7b7290195f2bb4bbca2061a3512bc6104a77f1b1
Anthony Curtis <tonydc1997@gmail.com>
2020-07-07 12:29:39 -0400

    feat: Import log for better debugging/errHandling

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+
 async function downloadLaunchData() {
   const response = await fetch("https://api.spacexdata.com/v4/launches", {
     method: "GET",
